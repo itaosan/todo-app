d0aa1bc271be3224b06cf19aacb9a4b5b8733a9e
itaosan <itaosan3@gmail.com>
2025-01-19 21:31:54 +0900

    🔧 fix: ESLintの設定を修正
    - globalsパッケージを追加
    
    - React用の設定を追加
    
    - TypeScript用の設定を修正

--- eslint.config.js
+++ eslint.config.js
@@ -7,11 +7,17 @@ import tseslint from 'typescript-eslint'
 export default tseslint.config(
   { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
+      globals: {
+        ...globals.browser,
+        React: true,
+      },
+      parserOptions: {
+        project: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
     },
     plugins: {
       'react-hooks': reactHooks,
